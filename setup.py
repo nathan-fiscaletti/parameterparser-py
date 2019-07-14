164f09c3b953ac70171c5edb0412a6ae16bed87d
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2019-07-13 19:37:51 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r") as fh:
 
 setup(
     name="parameterparser",
-    version="0.0.1",
+    version="0.1.2",
     author="Nathan Fiscaletti",
     author_email="nathan.fiscaletti@gmail.com",
-    description="An advanced parameter-parser for Python.",
+    description="An advanced parameter parser for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nathan-fiscaletti/parameterparser-py",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2.7+",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
